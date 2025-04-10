d316ad4b9bcf47b5632c97bd46319756bf6c6682
hobo-larry <47593826+hobo-larry@users.noreply.github.com>
2025-04-10 14:20:47 +0100

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+import Avatar from "./Avatar";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Avatar img="https://imgs.search.brave.com/AAIVdKD4U3US1KHAnHS8HLHcR2uf1Fn5gME0i3vspho/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9taXJv/Lm1lZGl1bS5jb20v/djIvMSo4T2tkTHB3/XzdWb2ttU3J6d1hM/bmJnLmpwZWc" />
+
+      <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      />
+    </div>
+  );
+}
+
+export default App;
